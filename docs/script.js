1259feab230d9b3cf98f51da793244e03a35c76d
geowatson <g.poputnikov@innopolis.ru>
2018-11-24 14:16:49 +0300

    added new styles

--- docs/script.js
+++ docs/script.js
@@ -7,6 +7,19 @@ const getData = async (id, date) => {
     return await q.json();
 };
 
+function start_rotate(id) {
+    let el = document.getElementById(`content${id}`);
+    let e = document.getElementById(`back${id}`);
+    e.style.transform = "rotateY(0deg)";
+    el.style.transform = "rotateY(180deg)";
+}
+
+function end_rotate(id) {
+    let el = document.getElementById(`content${id}`);
+    let e = document.getElementById(`back${id}`);
+    e.style.transform = "rotateY(180deg)";
+    el.style.transform = "rotateY(0deg)";
+}
 // init all
 
 for (let i = 1; i < 11; ++i) {
@@ -71,15 +84,50 @@ for (let i = 1; i < 11; ++i) {
             input = "elvirka privet)))))))";
             output = "";
         }
+        let code = "";
+        switch (i) {
+            case 1: code = `SELECT *\nFROM Self_driving_car\nWHERE color = 'red' AND license_plate LIKE 'AN%'`; break;
+            case 2: code = `SELECT strftime('%H',datetime) || 'h-' || strftime('%H',time(datetime, '+1 hour')) || 'h: ' || count(*) AS OUTPUT\nFROM Charge_log\nWHERE date(datetime) = ? -- date parameter\nGROUP BY strftime('%H',datetime)`; break;
+            case 3: code = `SELECT CAST(ROUND(CAST(SUM(Morning)*100 AS float)/(SELECT CAST(COUNT(*) AS float) FROM Self_driving_car))AS int) AS Morning,\nCAST(ROUND(CAST(SUM(Afternoon)*100 AS float)/(SELECT CAST(COUNT(*) AS float) FROM Self_driving_car))AS int) AS Afternoon,\nCAST(ROUND(CAST(SUM(Evening)*100 AS float)/(SELECT CAST(COUNT(*) AS float) FROM Self_driving_car))AS int) AS Evening\nFROM (SELECT car, MAX(CASE WHEN\n(\n(\ntime(datetime) >= time('07:00') and\ntime(datetime) <= time('10:00')\n)\nor\n(\ntime(datetime, '+% minute' % trip_duration) >= time('07:00') and\ntime(datetime, '+% minute' % trip_duration) <= time('10:00')\n)\nor\n(\ntime(datetime) <= time('07:00') and\ntime(datetime, '+% minute' % trip_duration) >= time('10:00')\n)\n) THEN 1 ELSE 0 END) AS Morning,\nMAX(CASE WHEN\n(\n(\ntime(datetime) >= time('12:00') and\ntime(datetime) <= time('14:00')\n)\nor\n(\ntime(datetime, '+% minute' % trip_duration) >= time('12:00') and\ntime(datetime, '+% minute' % trip_duration) <= time('14:00')\n)\nor\n(\ntime(datetime) <= time('12:00') and\ntime(datetime, '+% minute' % trip_duration) >= time('14:00')\n)\n) THEN 1 ELSE 0 END) AS Afternoon,\nMAX(CASE WHEN\n(\n(\ntime(datetime) >= time('17:00') and\ntime(datetime) <= time('19:00')\n)\nor\n(\ntime(datetime, '+% minute' % trip_duration) >= time('17:00') and\ntime(datetime, '+% minute' % trip_duration) <= time('19:00')\n)\nor\n(\ntime(datetime) <= time('17:00') and\ntime(datetime, '+% minute' % trip_duration) >= time('19:00')\n)\n) THEN 1 ELSE 0 END) AS Evening\nFROM "Order"\nWHERE date(datetime) >= date(?) and -- date parameter\ndate(datetime) < date(?, '+7 day') -- date parameter\nGROUP BY car)`; break;
+            case 4: code = ""; break;
+            case 5: code = `SELECT CAST("AVG"(distance_to_user) AS INT) AS [Average distance (m)],\nCAST("AVG"(trip_duration) AS INT) AS [Average trip duration (min)]\nFROM "Order"\nWHERE date(datetime) = ?`; break;
+            case 6: code = `select tipe, time, location\nfrom (\nselect * from (\nselect 'pick-up' as tipe, 'Morning' as time, "from" as location, 1 as ord\nfrom "Order"\nwhere time(datetime) >= time('07:00') and time(datetime) <= time('10:00')\ngroup by 3\norder by count(*) desc\nlimit 3\n)\nunion\nselect * from (\nselect 'pick-up' as tipe, 'Afternoon' as time, "from" as location, 2 as ord\nfrom "Order"\nwhere time(datetime) >= time('12:00') and time(datetime) <= time('14:00')\ngroup by 3\norder by count(*) desc\nlimit 3\n)\nunion\nselect * from (\nselect 'pick-up' as tipe, 'Evening' as time, "from" as location, 3 as ord\nfrom "Order"\nwhere time(datetime) >= time('17:00') and time(datetime) <= time('19:00')\ngroup by 3\norder by count(*) desc\nlimit 3\n)\nunion\nselect * from (\nselect 'destination' as tipe, 'Morning' as time, "to" as location, 1 as ord\nfrom "Order"\nwhere time(datetime) >= time('07:00') and time(datetime) <= time('10:00')\ngroup by 3\norder by count(*) desc\nlimit 3\n)\nunion\nselect * from (\nselect 'destination' as tipe, 'Afternoon' as time, "to" as location, 2 as ord\nfrom "Order"\nwhere time(datetime) >= time('12:00') and time(datetime) <= time('14:00')\ngroup by 3\norder by count(*) desc\nlimit 3\n)\nunion\nselect * from (\nselect 'destination' as tipe, 'Evening' as time, "to" as location, 3 as ord\nfrom "Order"\nwhere time(datetime) >= time('17:00') and time(datetime) <= time('19:00')\ngroup by 3\norder by count(*) desc\nlimit 3\n)\norder by ord\n)`; break;
+            case 7: code = `select car, count(*) as count\nfrom "Order"\nwhere date(datetime) > date('now', '-3 month')\ngroup by car\norder by count\nlimit cast(0.1*(select count(*) from Self_driving_car) as int)`; break;
+            case 8: code = `select user, sum(C) as amount from\n(\nselect * from "Order"\ninner join\n(\nselect count(car) as C, car, datetime as dt from Charge_log\ngroup by strftime('%m %d', datetime), car\norder by datetime and car asc\n) as C\non (strftime('%m %d', dt) = strftime('%m %d', datetime))\nwhere date(datetime) >= ? & date(datetime) <= date(?, '+1 month')\ngroup by strftime('%m %d', dt)\n)\ngroup by user;`; break;
+            case 9: code = `select workshop, cast(max(C) / 4.289 as int) as average, T as part\nfrom\n(\nselect type as T, count(type) as C, *\nfrom Repair_log\ninner join Repair_part_log Rpl on Repair_log.lid = Rpl.lid\ninner join Car_part Cp on Rpl.part = Cp.pid\ngroup by type, workshop\norder by workshop asc\n)\ngroup by workshop`; break;
+            case 10: code = `select type\nfrom\n(\nselect type, avg(C.price) + avg(R.price) as avg_price\nfrom Self_driving_car\ninner join Repair_log R on Self_driving_car.cid = R.car\ninner join Charge_log C on Self_driving_car.cid = C.car\ngroup by type\norder by avg_price desc\n)\nlimit 1`; break;
+        }
+        code = code
+            .split("\n")
+            .map(d => d
+                .replace(/((select)|(from)|(where)|(and)|(group by)|( as )|(limit)|(union)|(order by)|(SELECT)|(FROM)|(WHERE)|(AND)|(GROUP BY)|( AS )|(LIMIT)|(UNION)|(ORDER BY)|( = )|( != )|( >= )|( <= )|(\*))/g, '<div class="red">$1</div>')
+            )
+            .map((d, index) => `
+                                <div class="code">
+                                    <div style="position: absolute;display: inline-block; color: #ababab; word-wrap: normal;">
+                                        ${index + 1}
+                                    </div>
+                                    <div style="display: inline-block; margin-left: 20px;">
+                                        ${d}
+                                    </div>
+                                </div>`)
+            .join("");
 
-        let j = `in${i}`;
         document.getElementById('apocalypse').innerHTML =
             `
             ${document.getElementById('apocalypse').innerHTML}
             <div id="in${i}" class="card rounded" style="position: relative;">
-                <h1 style="margin-bottom: 10px">Query 3.${i}</h1>
-                ${input}
-                ${output}
+                <div id="content${i}" class="content rounded animated">
+                    <h1 style="margin-bottom: 10px">Query 3.${i}</h1>
+                    <div onclick="start_rotate('${i}')" style="position: absolute; right: 15px; top: 17px;font-size: 10px; color: #3657ae">Show code</div>
+                    ${input}
+                    ${output}
+                </div>
+                <div id="back${i}" class="card_back rounded animated">
+                    <div style="font-weight: bold;">Code:</div>
+                    <div onclick="end_rotate('${i}')" style="position: fixed; right: 15px; top: 17px;font-size: 10px; color: #3657ae">close</div>
+                    ${code}
+                </div>
             </div>
         `;
         // returns info that promise is handled
